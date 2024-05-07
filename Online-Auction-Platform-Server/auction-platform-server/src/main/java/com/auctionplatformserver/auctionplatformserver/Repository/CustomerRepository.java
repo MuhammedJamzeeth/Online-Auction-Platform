c96b9782c51aedf87ab90ca08cb614f6fd422b9d
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-05-07 15:36:03 +0530

    deleted

--- Online-Auction-Platform-Server/auction-platform-server/src/main/java/com/auctionplatformserver/auctionplatformserver/Repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.auctionplatformserver.auctionplatformserver.Repository;
-
-import com.auctionplatformserver.auctionplatformserver.Entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-}
