7a87fdd32f60bc54621ac3a2bee37523ff24ba4d
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-05-11 13:04:44 +0530

    update customer

--- online-auction-client/src/pages/Customer/Customer.js
+++ online-auction-client/src/pages/Customer/Customer.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Customer.css";
+import CustomerList from "../../components/Customer/CustomerList";
 import axios from "axios";
 
 const Customer = () => {
@@ -11,45 +11,17 @@ const Customer = () => {
 
   const fetchCustomers = async () => {
     try {
-      const response = await axios.get(
-        "http://localhost:8080/api/v1/auth/getallcustomers"
-      );
-      if (!response.ok) {
-        throw new Error("Failed to fetch customers");
-      }
-      setCustomers(response.data);
+        const response = await axios.get('http://localhost:8080/api/v1/auth/getallcustomers');
+        setCustomers(response.data);
     } catch (error) {
-      console.error("Error fetching customers:", error);
+        console.error("Error fetching customers:", error);
     }
   };
+  
 
   return (
     <>
-      <div className="cus-container-title">All Customers</div>
-      <div className="cus-container">
-        <table>
-          <thead>
-            <tr>
-              <th>Customer ID</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th>Address</th>
-            </tr>
-          </thead>
-          <tbody>
-            {customers.map((customer, index) => (
-              <tr key={index}>
-                <td>{customer.id}</td>
-                <td>{customer.firstName}</td>
-                <td>{customer.email}</td>
-                <td>{customer.phoneNumber}</td>
-                <td>{customer.address}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <CustomerList customers={customers} />
     </>
   );
 };
