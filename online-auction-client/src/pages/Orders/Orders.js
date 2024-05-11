8e39bd2f0ef32e079ac9efd353495871e28afe97
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-05-11 13:04:57 +0530

    update order

--- online-auction-client/src/pages/Orders/Orders.js
+++ online-auction-client/src/pages/Orders/Orders.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'; // Import axios
-import './Order.css';
+import axios from 'axios';
+import OrderList from '../../components/Order/OrderList';
 
 const Orders = () => {
     const [orders, setOrders] = useState([]);
@@ -11,8 +11,7 @@ const Orders = () => {
 
     const fetchOrders = async () => {
         try {
-            const response = await axios.get('http://localhost:8080/orders'); // Use axios.get instead of fetch
-            console.log('Orders:', response.data);
+            const response = await axios.get('http://localhost:8080/getallorders');
             setOrders(response.data);
         } catch (error) {
             console.error('Error fetching Orders:', error);
@@ -21,32 +20,7 @@ const Orders = () => {
 
     return (
         <>
-            <div className='order-container-title'>All Orders</div>
-            <div className="order-container">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Orders ID</th>
-                            <th>Product Name</th>
-                            <th>Category</th>
-                            <th>Customer</th>
-                            <th>Win Prize</th>
-                            <th>Order Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {orders.map((order, index) => (
-                            <tr key={index}>
-                                <td>{order.order_id}</td>
-                                <td>{order.orderName}</td>
-                                <td>{order.cusEmail}</td>
-                                <td>{order.cusPhone}</td>
-                                <td>{order.cusAddress}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+            <OrderList orders={orders} />
         </>
     );
 };
